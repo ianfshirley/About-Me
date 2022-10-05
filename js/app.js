e6f8819dd28f05bf0b870ef875d6d21114469bd5
Ian Shirley <ianshirley@Leylas-MacBook-Pro.local>
2022-10-04 21:42:48 -0700

    final touches

--- js/app.js
+++ js/app.js
@@ -27,35 +27,37 @@ alert("Hi " + username + "!");
 
 let pets = prompt('Do I have any pets?').toLowerCase();
 
-if(pets === 'yes' || pets === 'y') {
+if (pets === 'yes' || pets === 'y') {
   //console.log("That's right, I have a dog named Ramen & a cat named Sushi");
   alert("That's right, I have a dog named Ramen & a cat named Sushi");
 }
 
 let siblings = prompt('Do I have any siblings?').toLowerCase();
 
-if(siblings === 'yes' || siblings === 'y') {
+if (siblings === 'yes' || siblings === 'y') {
   //console.log('Yep, I have one sister who is 2 years older than me.');
   alert('Yep, I have one sister who is 2 years older than me.');
 }
 
 let kids = prompt('Do I have any kids?').toLowerCase();
 
-if(kids === 'no' || kids === 'n') {
+if (kids === 'no' || kids === 'n') {
   //console.log('Correct! I have 3 nephews and that's plenty!');
   alert('Correct! I have 3 nephews and that is plenty!');
 }
 
 let states = prompt('Do you think I have traveled to all 50 states?').toLowerCase();
 
-if(states === 'no' || states === 'n') {
+if (states === 'no' || states === 'n') {
   //console.log('Not even close, more like 10 or 15');
   alert('Not even close, more like 10 or 15');
 }
 
 let bones = prompt('Have I ever broken a bone?').toLowerCase();
 
-if(bones === 'yes' || bones === 'y') {
+if (bones === 'yes' || bones === 'y') {
   //console.log('Yes, I have broken 7 bones unfortunately');
   alert('Yes, I have broken 7 bones unfortunately');
 }
+
+alert('Thanks for checking out my page, ' + username + '!');
